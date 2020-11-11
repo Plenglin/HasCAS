177177c05e584e3148f170f815e01b66f3048690
Astrid Yu <astrid@astrid.tech>
2020-11-11 12:33:13 -0800

    i'm in abstract algebra hell

--- src/HasCAS/Scalar.hs
+++ src/HasCAS/Scalar.hs
@@ -3,10 +3,12 @@
 module HasCAS.Scalar where
 
 
-class (Eq a, Ord a) => Ring a where 
+class (Show a, Eq a, Ord a) => Ring a where 
   (+) :: a -> a -> a
   (*) :: a -> a -> a
   (-) :: a -> a
+  idAdd :: a
+  idMul :: a
 
 class Ring a => DivRing a where
   (/) :: a -> a -> a
@@ -20,20 +22,28 @@ instance Eq Z where
   (Z a) == (Z b) = a == b
 instance Ord Z where 
   compare (Z a) (Z b) = compare a b
+instance Show Z where 
+  show (Z x) = show x
 instance Ring Z where
   (Z a) + (Z b) = Z (a Prelude.+ b)
   (Z a) * (Z b) = Z (a Prelude.* b)
   (-) (Z a) = Z (-a)
+  idAdd = Z 0
+  idMul = Z 1
 
 data Q = Q Rational
 instance Eq Q where 
   (Q a) == (Q b) = a == b
 instance Ord Q where 
   compare (Q a) (Q b) = compare a b
+instance Show Q where 
+  show (Q x) = show x
 instance Ring Q where 
   (Q a) + (Q b) = Q (a Prelude.+ b)
   (Q a) * (Q b) = Q (a Prelude.* b)
   (-) (Q a) = Q (-a)
+  idAdd = Q 0
+  idMul = Q 1
 instance DivRing Q where 
   (Q a) / (Q b) = Q (a Prelude.+ b)
   (!) (Q a) = Q (1 Prelude./ a)
@@ -43,10 +53,14 @@ instance Eq R where
   (R a) == (R b) = a == b
 instance Ord R where 
   compare (R a) (R b) = compare a b
+instance Show R where 
+  show (R x) = show x
 instance Ring R where 
   (R a) + (R b) = R (a Prelude.+ b)
   (R a) * (R b) = R (a Prelude.* b)
   (-) (R a) = R (-a)
+  idAdd = R 0
+  idMul = R 1
 instance DivRing R where 
   (R a) / (R b) = R (a Prelude.+ b)
   (!) (R a) = R (1 Prelude./ a)
